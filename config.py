70ec48e24ae6b7dbffecf0dbbf458abe3019be50
Mohamed Amri <mohamed.amri@alpheya.com>
2025-08-24 14:02:33 +0200

    WIP

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     logfire_token:          str | None = Field(..., alias="LOGFIRE_TOKEN")
     mcp_server_url:         str | None = Field(..., alias="MCP_SERVER_URL")
     openai_api_key:         str | None = Field(..., alias="OPENAI_API_KEY")
-    openai_url:             str | None = Field(..., alias="OPENAI_URL")
     temporal_server_url:    str | None = Field(..., alias="TEMPORAL_SERVER_URL")
 
 settings = Settings()
